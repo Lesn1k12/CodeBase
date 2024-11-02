c7f6de618399502f8119da069c3be35923656eb2
Lesn1k12 <m.tkhorevskyi@gmail.com>
2024-11-02 21:20:47 +0100

    init

---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+
+import Dashboard from './pages/dashboard';
+import ErrorPage from './error-page';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/repos",
+    element: <div className=""></div>
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Login from '../components/login/Login'
+import Register from '../components/register/Register'
+
+const AuthPage = () => {
+  return (
+    <div>authPage</div>
+  )
+}
+
+export default AuthPage

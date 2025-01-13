19b83c49b9b07da85905d8467de6ed294397d546
Lesn1k12 <m.tkhorevskyi@gmail.com>
2025-01-13 16:27:15 +0100

    small updates

--- comment_Services/comment_Services/middleware/generatortoken.js
+++ comment_Services/comment_Services/middleware/generatortoken.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 function generateToken() {
-  const payload = { user: 'testUser' }; // Данные для токена
-  const secret = process.env.JWT_SECRET || 'your_default_jwt_secret'; // Используйте секрет из .env или дефолтный
+  const payload = { user: 'testUser' }; 
+  const secret = process.env.JWT_SECRET || 'nYSvA9hsWvSZT/AOMcmiNze/YGtkwEFUMfCbos0LTgM='; 
 
-  const token = jwt.sign(payload, secret, { expiresIn: '1h' }); // Токен действует 1 час
-  console.log('Generated Token:', token); // Логируем токен
+  const token = jwt.sign(payload, secret, { expiresIn: '1h' }); 
+  console.log('Generated Token:', token); 
 }
 
 generateToken();
